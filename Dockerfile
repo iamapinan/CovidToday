0ffecabedf0fd8cb9438f79bb49b41b1d621712c
Apinan Woratrakun <apinan@MacBook-Pro-2018.local>
2020-04-24 03:23:19 +0700

    initial

---
+++ Dockerfile
@@ -0,0 +1,11 @@
+FROM nginx:alpine
+
+MAINTAINER Apinan
+
+COPY ./build /usr/share/nginx/html
+
+WORKDIR /usr/share/nginx/html
+
+EXPOSE 80
+
+CMD ["nginx", "-g", "daemon off;"]
--- src/App.js
+++ src/App.js
@@ -1,26 +1,47 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import logo from './logo.png';
 import './App.css';
+import axios from 'axios';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+export default class App extends Component {
+  constructor (props) {
+    super()
+    this.state = {
+      today: {}
+    }
+  }
+  getcovid () {
+    axios.get('https://covid19.th-stat.com/api/open/today')
+    .then(rs => rs.data)
+    .then(covid => {
+      this.setState({ today: covid })
+    })
+  }
+
+  componentDidMount() {
+    this.getcovid()
+  }
+
+  render () {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Covid 19 New case today is <span className='text-danger'>{this.state.today.NewConfirmed}</span> people.
+          <br/>Total case <span className='text-warning'>{this.state.today.Confirmed}</span> and recovered <span className='text-success'>{this.state.today.Recovered}</span></p>
+          
+        </header>
+        <p>Last updated {this.state.today.UpdateDate} <br/> Data source from&nbsp;
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://covid19.th-stat.com/"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+          https://covid19.th-stat.com/
+        </a> <br/>This project is opensource <a href="https://gitlab.com/apinan/covidetoday" target="_blank" class="App-link">https://gitlab.com/apinan/covidetoday</a></p>
+      </div>
+    );
+  }
 }
-
-export default App;
