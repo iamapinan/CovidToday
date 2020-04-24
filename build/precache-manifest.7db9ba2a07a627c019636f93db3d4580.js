85e623541e7b5005629f474aa6ace065dd8a83fa
Apinan Woratrakun <apinan@MacBook-Pro-2018.local>
2020-04-24 16:03:26 +0700

    I forget build

---
+++ build/precache-manifest.7db9ba2a07a627c019636f93db3d4580.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "950754f8521587fa7eb5b5fb83b7daac",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5be18b0176a07d1c76bc",
+    "url": "/static/css/main.3b25eb2f.chunk.css"
+  },
+  {
+    "revision": "5dec99172f8dd1b8698b",
+    "url": "/static/js/2.f961fe2b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.f961fe2b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5be18b0176a07d1c76bc",
+    "url": "/static/js/main.2b16c39f.chunk.js"
+  },
+  {
+    "revision": "a84c77bd478b6a284889",
+    "url": "/static/js/runtime-main.d900fefa.js"
+  },
+  {
+    "revision": "9118371084f92066f4952756684ca2f5",
+    "url": "/static/media/logo.91183710.png"
+  }
+]);
